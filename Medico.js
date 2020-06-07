5eb4cf375e33db90744b348273d91f9e59f5d296
Florence <46047968+Ogheneyore@users.noreply.github.com>
2020-06-07 13:32:26 +0100

    Medico
    MEPRIMS project submission for delta state

---
+++ Medico.js
@@ -0,0 +1,58 @@
+
+Delta: [
+            
+    LGA: [{name: "Aniocha North", value: "187"}, 
+            
+              {name: "Aniocha South", value: '188'},
+              
+              {name: "Bomadi", value: '189'},
+
+              {name: "Burutu", value: '190'},
+
+              {name: "Ethiope East", value: '191'},
+
+              {name: "Ethiope West", value: '192'},
+
+              {name: "Ika North East", value: '193'},
+
+              {name: "Ika South", value: '194'},
+
+              {name: "Isoko North", value: '195'},
+
+              {name: "Isoko South", value: '196'},
+
+              {name: "Idokwa East", value: '197'},
+
+              {name: "Idokwa West", value: '198'},
+
+              {name: "Okpe", value: '199'},
+
+              {name: "Oshimili North", value: '200'},
+
+              {name: "Oshimili South", value: '201'},
+
+              {name: "Patani", value: '202'},
+
+              {name: "Sapele", value: '203'},
+
+              {name: "Udu", value: '204'},
+
+              {name: "Ughelli North", value: '205'},
+
+              {name: "Ughelli South", value: '206'},
+
+              {name: "Ukwuani", value: '207'},
+
+              {name: "Uvwie", value: '208'},
+
+              {name: "Warri North", value: '209'},
+
+              {name: "Warri South", value: '210'},
+
+              {name: "Warri South West", value: '211'},
+              
+              ]} 
+               
+
+],
+
